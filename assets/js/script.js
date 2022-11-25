0e4add764be2f101314c9e365f3159be0463f979
Nicko Balute <96706430+ByteChee12@users.noreply.github.com>
2022-11-25 17:04:36 +0800

    Add Header, Navbar , Hero SECTION

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+
+
+
+
+/**
+  * PRELOADING
+  */
+
+  const loadElement = document.querySelector("[data-preloader]");
+
+
+  window.addEventListener("load", function() {
+  	loadElement.classList.add("loaded");
+  });
+
+
+/**
+  * # MOBILE NAVBAR TOGGLE
+  */
+
+
+  const navbar = document.querySelector("[data-navbar]");
+  const navToggler = document.querySelector("[data-nav-toggler]");
+
+  const	toggleNavbar = function() {
+  	navbar.classList.toggle("active");
+  	navToggler.classList.toggle("active");
+  }
+
+
+  navToggler.addEventListener('click', toggleNavbar);
+
+
+  /**
+    * HEADER
+    */
+
+  const header = document.querySelector("[data-header]");
+
+  window.addEventListener("scroll", function(){
+  	if(window.scrollY >= 100) {
+  		header.classList.add("active");
+  	} else {
+  		header.classList.remove("active");
+  	}
+  });
